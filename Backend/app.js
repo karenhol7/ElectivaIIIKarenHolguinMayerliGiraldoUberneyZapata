4f52b4e9c41fe6fe32f8056da2ef9641ae752b63
Karen Holguin <karenhol137@gmail.com>
2024-09-16 20:17:57 -0500

    ADD Backend Dockerfile refactor

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ const setRoutes = require('./routes');
 const swagger = require('./swagger')
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: false}));
